d53741072b785bc13a4ab4c736225981a7f6738c
sunadoi <y.sunadoi@gmail.com>
2019-12-09 17:42:21 +0900

    Update show.js tab-active

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,10 +1,13 @@
 $(function () {
   $('.tab-content').hide();
   $('.tab-content').eq(0).show();
+  $('.tab-button').eq(0).addClass('active');
 
   $('.tab-button').each(function () {
     $(this).on('click', function () {
       var index = $('.tab-button').index(this);
+      $('.active').removeClass('active');
+      $(this).addClass('active');
       $('.tab-content').hide();
       $('.tab-content').eq(index).show();
     });
