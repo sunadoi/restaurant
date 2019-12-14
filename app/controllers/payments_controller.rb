b6eab2b5aef73306e6e6b24345a7ed70bed6b84d
sunadoi <y.sunadoi@gmail.com>
2019-12-14 13:34:01 +0900

    Update payments_controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -19,30 +19,30 @@ class PaymentsController < ApplicationController
       
       #決済記録を作成
       # stripeのcheckoutフォームから送られてきたパラメーターでpaymentのインスタンスを作成
-      payment = Payment.new
-
-      payment.email = customer.email # 支払った人がstripeのcheckoutフォームに入力したemail(支払い完了後、stripeからこのメールアドレスに支払い完了メールが送られる)
-      payment.description = charge.description #決済の概要
-      payment.currency = charge.currency  #通貨
-      payment.customer_id = customer.id   # stripeのcustomerインスタンスのID
-      payment.payment_date = Time.current # payment_date(支払いを行った時間)は現在時間を入れる
-      payment.uuid = SecureRandom.uuid  # 請求書の番号としてuuidを用意する
-      payment.charge_id = charge.id  # 返金(refund)の時に使うchargeのIDをpaymentに保存しておく
-      payment.stripe_commission = (charge.amount * 0.036).round  # stripeの手数料(3.6%)分の金額
-      payment.amount_after_subtract_commission = charge.amount - payment.stripe_commission  # stripeの手数料(3.6%)分を引いた金額(依頼者が払った96.4%の金額)
-      payment.receipt_url = charge.receipt_url  # この決済に対するstripeが用意してくれる領収書のURL
+      payment = Payment.new(
+        email: customer.email, # 支払った人がstripeのcheckoutフォームに入力したemail(支払い完了後、stripeからこのメールアドレスに支払い完了メールが送られる)
+        description: charge.description, #決済の概要
+        currency: charge.currency, #通貨
+        customer_id: customer.id, # stripeのcustomerインスタンスのID
+        payment_date: Time.current, # payment_date(支払いを行った時間)は現在時間を入れる
+        uuid: SecureRandom.uuid, # 請求書の番号としてuuidを用意する
+        charge_id: charge.id, # 返金(refund)の時に使うchargeのIDをpaymentに保存しておく
+        stripe_commission: (charge.amount * 0.036).round, # stripeの手数料(3.6%)分の金額
+        amount_after_subtract_commission: charge.amount - (charge.amount * 0.036).round, # stripeの手数料(3.6%)分を引いた金額(依頼者が払った96.4%の金額)
+        receipt_url: charge.receipt_url # この決済に対するstripeが用意してくれる領収書のURL
+      )
       payment.save!
 
       #フォームから送られてきたパラメータをもとに予約情報を作成
-      @reservation = Reservation.new
-
-      @reservation.user_name = params[:user_name]
-      @reservation.reservation_date = params[:reservation_date]
-      @reservation.menu_id = params[:menu_id]
-      @reservation.count = params[:count]
-      @reservation.amount = params[:amount]
-      @reservation.request = params[:request]
-      @reservation.payment_id = payment.id
+      @reservation = Reservation.new(
+        user_name: params[:user_name],
+        reservation_date: params[:reservation_date],
+        menu_id: params[:menu_id],
+        count: params[:count],
+        amount: params[:amount],
+        request: params[:request],
+        payment_id: payment.id
+      )
       @reservation.save!
 
       redirect_to complete_reservation_path(id: @reservation.id)
