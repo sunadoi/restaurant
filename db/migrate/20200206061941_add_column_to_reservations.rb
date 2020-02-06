c50af7336139f61be1909b79b60dd953b14bb865
sunadoi <y.sunadoi@gmail.com>
2020-02-06 15:30:53 +0900

    Update 20200206061941_add_column_to_reservations.rb

--- db/migrate/20200206061941_add_column_to_reservations.rb
+++ db/migrate/20200206061941_add_column_to_reservations.rb
@@ -1,6 +1,5 @@
 class AddColumnToReservations < ActiveRecord::Migration[5.2]
   def change
     add_column :reservations, :user_name, :string
-    add_column :reservations, :request, :text
   end
 end
