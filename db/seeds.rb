b6d45da57ee506c7be9d738527aceef1a8c05a14
sunadoi <y.sunadoi@gmail.com>
2020-02-06 15:49:26 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Menu.create!(
+  name: "席のみ",
+  price: '0',
+)
+
+Menu.create!(
+  name: "Aコース",
+  price: '3000',
+)
+
+Menu.create!(
+  name: "Bコース",
+  price: '4500',
+)
+
+Menu.create!(
+  name: "Cコース",
+  price: '6000',
+)
+
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
